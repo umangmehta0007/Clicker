0f3e4294c22ea8b60e0c53f0e0056ff3e378ae33
Umang Mehta <mehta.umang@icloud.com>
2026-02-03 13:45:09 -0600

    ->Notify was successfully implemented
    -> Inside of notify, all elements were removed successfully first and then inside that new div created a String was added to be displayed.

--- src/View/CompanyView.ts
+++ src/View/CompanyView.ts
@@ -1,25 +1,29 @@
 import type Company from "../Model/Company.ts";
+// import type CompanyController from "../Controller/CompanyController.ts";
 
 export default class  CompanyView{
 
     #company: Company;
-    #teamEl: HTMLUListElement;
+    #teamEl: HTMLDivElement;
 
 
     constructor(company: Company){
         this.#company = company;
 
         this.#company.registerListener(this);
-
+        // document.querySelector('#app')!.innerHTML =
+        //     "<div id = 'company' ><ul></ul> </div>"
         document.querySelector('#app')!.innerHTML =
-            "<div id = 'company' ><ul></ul> </div>"
+            "<div id = 'company'> </div>"
 
-        this.#teamEl = document.querySelector("#company> ul");
+        this.#teamEl = document.querySelector("#company");
     }
 
     notify(){
 
-        console.log(this.#company.totalGifts());
+        this.#teamEl.replaceChildren();
+
+        this.#teamEl.innerHTML = `<strong>${'Total Gifts Delivered: '+this.#company.totalGifts()}</strong>`
     }
 
 }

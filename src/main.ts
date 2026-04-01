9455c7d35a05c032db949b54570bc0c734094175
Umang Mehta <mehta.umang@icloud.com>
2026-04-01 01:42:48 -0500

    File was loaded form the training data set and matricies were created from the raw string

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,41 @@
-
 import HomePageController from "./Controller/HomePageController.ts";
 import ddl from '../create-tables.sql?raw'
+import csv from 'src/model.csv?raw'
 import db from './Model/connection.ts'
 // load the tables into the database:
+
+function exec(csv:string) {
+    let matrix: number[][] = [];
+    const lines = csv.trim().split("\n");
+
+    for (let line of lines) {
+        const parts = line.split(",");
+
+        const row: number[] = [];
+        for (let value of parts) {
+            row.push(Number(value));
+        }
+
+        matrix.push(row);
+    }
+    return matrix;
+}
+
+function dexec(numerator: number[][]): number[] {
+    const denominator: number[] = [];
+
+    for (let row of numerator) {
+        let sum = row.reduce((acc, val) => acc + val, 0);
+        denominator.push(sum);
+    }
+
+    return denominator;
+}
+const numerator = exec(csv);
+const denominator = dexec(numerator);
 db().exec(ddl)
 
-
-new HomePageController();
+/*
+Let's work form here
+ */
+new HomePageController(numerator, denominator);
